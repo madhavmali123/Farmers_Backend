d3aa2a7a3f951d02ae98dbf1a1db753e28dbb5d6
Madhav mali <madhavmali191103@gmail.com>
2025-10-01 17:43:04 +0530

    .

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const { User, Product, Cart } = require("./Models/models");
 const { Crop } = require("./Models/cropModels");
+
 const multer = require("multer");
 const cloudinary = require("cloudinary").v2;
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
+
 const path = require("path");
 const fs = require("fs");
 const Razorpay = require("razorpay");
@@ -13,8 +15,10 @@ const Razorpay = require("razorpay");
 const dotenv = require("dotenv");
 dotenv.config();
 
-//key_id = rzp_test_RK86XGnm6WU2nA
-//secret = xOCaUVHGiF0GGr3bDBgKInJN
+app.use(express.json({ limit: "20mb" }));
+app.use(express.urlencoded({ limit: "20mb", extended: true }));
+
+
 
 const app = express();
 const razorpay = new Razorpay({
@@ -28,15 +32,11 @@ app.use(express.json());
 app.use("/uploads", express.static("uploads"));
 
 
-
-app.use(cors());
-
-
-// app.use(cors({
-//   origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
-//   methods: ["GET", "POST", "PUT", "DELETE"],
-//   credentials: true
-// }));
+app.use(cors({
+  origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 // connect to MongoDB
 mongoose.connect(process.env.mongo_uri, {
   useNewUrlParser: true,
